78ba6c7f5e4fb50bfc190ae03f51f44b4bffa418
Marina Golao Vale <marinasgv@hotmail.com>
2022-01-07 15:47:37 -0300

    Revisão

---
+++ Revisao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+Console.WriteLine("Informe a opção desejada: ");
+Console.WriteLine("1 - Inserir novo aluno");
+Console.WriteLine("2 - Listar alunos");
+Console.WriteLine("3 - Calcular média geral");
+Console.WriteLine("X");
+Console.WriteLine();
+
+string opcaoUsuario = Console.ReadLine();
+
+while (opcaoUsuario.ToUpper() != "X")
+{
+    switch (opcaoUsuario)
+    {
+        case "1":
+            //Adicionar alunos
+            break;
+        case "2":
+            //Listar alunos
+            break;
+        case "3":
+            //Média geral
+            break;
+        default:
+        //Esta fora das opcoes
+            throw new ArgumentOutOfRangeException();
+    }
+
+    Console.WriteLine("Informe a opção desejada: ");
+    Console.WriteLine("1 - Inserir novo aluno");
+    Console.WriteLine("2 - Listar alunos");
+    Console.WriteLine("3 - Calcular média geral");
+    Console.WriteLine("X");
+    Console.WriteLine();
+
+    Console.ReadLine();
+
+}
