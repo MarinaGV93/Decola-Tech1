be67cc0e1ab64b42bd0b6bdd4f66a8f285fc598a
Marina Golao Vale <marinasgv@hotmail.com>
2022-01-24 15:07:03 -0300

    Array

---
+++ Colecoes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Colecoes{
+    class Program{
+        static void Main(string[] args){
+            //Criando um array
+            int[] arrayInteiros = new int [3];
+
+            //Armazenando no array
+            arrayInteiros[0] = 10; //1
+            arrayInteiros[1] = 20; //2
+            arrayInteiros[2] = 30; //3
+
+            //Imprimir
+            System.Console.WriteLine("Com FOR");
+            for (int i = 0; i  < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+
+            System.Console.WriteLine("Com FOREACH");
+            foreach (int item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
